e5cf89e73c52c4f5d50eb161cd0f98b2627dc9f4
Fabio <akki.fabio@gmail.com>
2022-07-22 13:05:13 -0300

    Enviando Exercícios Collections

---
+++ CStack/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using static CQueue.Program;
+
+namespace CStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World Pilha!");
+            Console.WriteLine("----------------");
+            Console.WriteLine("Pilha de livros!");
+            Console.WriteLine("----------------\n");
+
+            Stack<Livro> pilhaLivros = new Stack<Livro>();
+
+            pilhaLivros.Push(new Livro("In Search of Lost Time", "Marcel Proust", 4215));
+            pilhaLivros.Push(new Livro("Ulysses", "James Joyce", 730));
+            pilhaLivros.Push(new Livro("Don Quixote", "Miguel de Cervantes", 1072));
+
+            Console.WriteLine($"Primeiro livro do topo da stack: {pilhaLivros.Peek()}");
+            pilhaLivros.Pop(); // retira o livro no topo da stack (último a ser adicionado)
+            Console.WriteLine("--Removido o livro no topo da stack--");
+            Console.WriteLine($"Quandidade atual de livros: {pilhaLivros.Count}");
+
+            
+
+            
+
+            foreach (var item in pilhaLivros)
+            {
+                Console.WriteLine($"{item.ToString()}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
